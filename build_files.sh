1c4d819360fc4a65f386586afaecdafd37f58000
ReevuChatterjee <chatterjeeireevu@gmail.com>
2025-09-19 09:16:18 +0530

    Second Deploy

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,13 @@
 echo "BUILD START"
+
+# Ensure pip is available
+python3.9 -m ensurepip --upgrade
+
+# Install dependencies
+python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
+
+# Collect static files
 python3.9 manage.py collectstatic --noinput --clear
+
 echo "BUILD END"
